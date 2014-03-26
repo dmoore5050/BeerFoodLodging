d5e6c5983bb09aa8ce564ccdec43ce3759b2d644
dmoore5050 <dustinmoore@gmail.com>
2014-03-26 18:38:48 -0500

    Add cuke tests/step defs

--- features/step_definitions/step_definitions.rb
+++ features/step_definitions/step_definitions.rb
@@ -1,18 +1,45 @@
-Given(/^the user '(.*?)'\/'(.*?)' with password of '(.*?)'$/) do |username, email, password|
-  User.create(username: username, email: email, password: password, password_confirmation: password)
-end
-
-Given(/^the user '(.*?)' is logged in$/) do |username|
-  user = User.where(username: username).first
-  login user
-end
-
-When(/^I go to the homepage$/) do
+Given(/^the user '(.*?)' with password '(.*?)' presses '(.*?)'$/) do |username, pw, text|
+  User.create(username: username, email: "#{username}@email.com", password: pw, password_confirmation: pw)
   visit '/'
+  click_link_or_button text
+end
+
+Given(/^the user '(.*?)' logs in with password '(.*?)'$/) do |username, pw|
+  User.create(username: username, email: "#{username}@email.com", password: pw, password_confirmation: pw)
+  visit '/users/sign_in'
+  fill_in 'Login', with: username
+  fill_in 'Password', with: pw
+  click_link_or_button 'Sign in'
+end
+
+Given(/^a logged in admin$/) do
+  username, password = 'admin', '12345678'
+  user = User.create(username: username, email: "#{username}@email.com", password: password, password_confirmation: password)
+  user.update_column :admin, true
+  visit '/users/sign_in'
+  fill_in 'Login', with: username
+  fill_in 'Password', with: password
+  click_link_or_button 'Sign in'
+end
+
+Given(/^I am on the homepage$/) do
+  visit '/'
+end
+
+Given(/^(.*?) (.*?) (?:message|messages)$/) do |num, state|
+  user = User.all.first
+  (1..num.to_i).each do |num|
+    msg  = Message.create(title: "title#{num}", body: "body#{num}", reason: "reason#{num}", user: user)
+    msg.update_column :read, true if state == 'read'
+  end
 end
 
 When(/^I (?:click|press) '(.*?)'$/) do |text|
   click_link_or_button text
+end
+
+When(/^I (?:click|press) the header '(.*?)'$/) do |text|
+  find('h3', text: 'text').click
 end
 
 When(/^I choose '(.*?)' from '(.*?)'$/) do |option, field_name|
@@ -21,6 +48,10 @@ end
 
 When(/^I fill in '(.*?)' for '(.*?)'$/) do |text, field_name|
   fill_in field_name, with: text
+end
+
+When(/^I hover over '(.*?)'$/) do |name|
+  find('.nav_item', text: 'About').hover
 end
 
 When(/^show me the page$/) do
---
+++ features/user_can_view_about_page.feature
@@ -0,0 +1,53 @@
+Feature: User can use about menu
+  In order to learn about BFL
+  As a user
+  I want to be able to view the about page
+
+  Background:
+    Given the user 'bob' logs in with password 'password1'
+      And I am on the homepage
+      And I hover over 'About'
+
+  @javascript
+  Scenario: View About page
+    When I click 'About BFL'
+    Then I should see 'About BeerFoodLodging'
+      And I should see 'info@beerfoodlodging.com'
+
+  @javascript
+  Scenario: View FAQs
+    When I click 'FAQs'
+    Then I should see 'Frequently Asked Questions'
+
+  @wip
+  @javascript
+  Scenario: View FAQ Answer
+    When I click 'FAQs'
+      And I click the header 'question1'
+    Then I should see 'answer1'
+
+  @javascript
+  Scenario: View Subscription Levels
+    When I click 'Subscription Info'
+    Then I should see 'Premium'
+      And I should see 'Custom Itinerary'
+
+  @wip
+  @javascript
+  Scenario: Subscribe to BFL
+    When I click 'Subscription Info'
+      And I press 'Upgrade to Premium'
+    Then I should see 'Paypal page'
+
+  @javascript
+  Scenario: Email site owner
+    When I click 'About BFL'
+      And I click 'Contact us'
+      And I fill in 'Praise' for 'Reason'
+      And I fill in 'Wow!' for 'Title'
+      And I fill in 'What a great site!' for 'Message'
+      And I press 'Submit'
+    Then I should see 'Message sent!'
+      And I should see 'Welcome to BFL!'
+
+
