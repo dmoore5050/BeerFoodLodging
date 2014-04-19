53c9ec59ec88b6164b8863e5a62923523d3eb475
dmoore5050 <dustinmoore@gmail.com>
2014-04-19 12:01:20 -0500

    added category model/contro/etc

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Category, 'associations' do
+  it { should have_many :posts }
+end
+
+describe Category, 'columns' do
+  it { should have_db_column :name }
+end
+
+describe Category, 'validations' do
+  it { should validate_presence_of :name }
+end
