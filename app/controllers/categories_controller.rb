d5eff99c007fe7d77719d36e0957ca433db3ee84
dmoore5050 <dustinmoore@gmail.com>
2014-04-19 12:12:19 -0500

    added views/cont methods for category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,22 @@
 class CategoriesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def update
+  end
 end
