e7a83327d501f906714e90199061846227becf28
dmoore5050 <dustinmoore@gmail.com>
2014-07-04 17:46:07 -0400

    update post controller/model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,64 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.create post_params
+
+    flash[:notice] = 'Post created!'
+
+    redirect_to posts_url
   end
 
   def show
   end
 
   def edit
+    @post = Post.find params[:id]
   end
 
   def update
+    @post = Post.find params[:id]
+
+    @post.update_attributes post_params
+
+    flash[:notice] = 'Post updated!'
+
+    redirect_to posts_url
   end
 
   def index
   end
 
-  def update
+  def destroy
+    post = Post.find params[:id]
+    post.destroy
+
+    flash[:notice] = 'Post destroyed!'
+
+    redirect_to posts_url
   end
+
+  private
+
+  def post_params
+    params
+      .require(:post)
+      .permit(:title, :body, :category_id, :tag_list, :published_on)
+      .merge(user: current_user)
+  end
+
+  def post
+    @post = Post.new
+  end
+  helper_method :post
+
+  def posts
+    @posts = Post.all
+  end
+  helper_method :posts
+
+  def category_list
+    @category_list = Category.all
+  end
+  helper_method :category_list
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
+  acts_as_taggable
+
   belongs_to :user
   belongs_to :category
   has_many   :comments
@@ -6,4 +8,10 @@ class Post < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :body
   validates_presence_of :category_id
+
+  def category
+    @category = Category.find( self.category_id )
+  rescue ActiveRecord::RecordNotFound
+    self.update_attribute :category_id, nil
+  end
 end
