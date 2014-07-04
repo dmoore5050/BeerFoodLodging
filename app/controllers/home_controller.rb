10afbb3a0ab4843780ee297eaee8779338d8de9e
dmoore5050 <dustinmoore@gmail.com>
2014-07-04 17:45:01 -0400

    update home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
+  def home
+    @posts = Post.where("posts.published_on IS NOT NULL").reverse_order.page(params[:page]).per(1)
+  end
 
   def index
+    @posts = Post.where("posts.published_on IS NOT NULL").tagged_with(params[:tag]).reverse_order.page(params[:page]).per(1)
   end
 
   def about
@@ -14,4 +18,14 @@ class HomeController < ApplicationController
 
   private
 
+  def user(id)
+    user = User.find id
+    user.username
+  end
+  helper_method :user
+
+  def format_time(t)
+    "#{t.strftime("%A")}, #{t.strftime("%B")} #{t.strftime("%d")} @ #{t.strftime("%I").gsub(/^0/, '')}:#{t.strftime("%M")} #{t.strftime("%p")}"
+  end
+  helper_method :format_time
 end
