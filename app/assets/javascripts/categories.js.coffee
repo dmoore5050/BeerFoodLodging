6a621534dd1752419a1e17c64188ff2ffa6baa2b
dmoore5050 <dustinmoore@gmail.com>
2014-07-04 09:44:16 -0400

    converted cat views to use javascript

--- app/assets/javascripts/categories.js.coffee
+++ app/assets/javascripts/categories.js.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$(document).ready ->
+
+  $('body').on 'click', '#reveal_new_cat_form', (e) ->
+    e.preventDefault()
+
+    $(this).fadeOut( 0 )
+    $('#new_category').animate
+      height: '125px', 200
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,20 +3,65 @@ class CategoriesController < ApplicationController
   end
 
   def create
+    names = category_params[:name].split(',')
+
+    @categories = names.map do |n|
+      Category.create(name: n.strip)
+    end
+
+    flash[:notice] = "Categories created!"
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def show
   end
 
   def edit
+    @category = Category.find params[:id]
   end
 
   def update
+    @category = Category.find params[:id]
+
+    @category.update_attributes category_params
+
+    flash[:notice] = 'Category updated!'
+
+    redirect_to categories_url
   end
 
   def index
   end
 
-  def update
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+
+    flash[:notice] = 'Category destroyed!'
+
+    respond_to do |format|
+      format.js
+    end
   end
+
+  private
+
+  def category_params
+    params
+      .require(:category)
+      .permit(:name)
+  end
+
+  def category
+    @category = Category.new
+  end
+  helper_method :category
+
+  def categories
+    @category = Category.all
+  end
+  helper_method :categories
 end
