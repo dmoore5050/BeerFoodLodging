16af39a8411a159d6a9dc9e3ed060d2fb031b913
dmoore5050 <dustinmoore@gmail.com>
2014-07-04 17:41:10 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 BFL::Application.routes.draw do
-
   devise_for :users, controllers: { registrations: 'registrations' }
-  root to: 'home#index'
+
+  root to: 'home#home'
 
   match 'about' => 'home#about', via: :get
   match 'subscriptions' => 'home#subscriptions', via: :get
 
+  get 'tags/:tag', to: 'home#index', as: :tag
+
   resources :cities, only: [:new, :create, :show, :destroy]
   resources :faqs, only: [:new, :create, :index, :destroy]
-  resources :messages, only: [:new, :create, :destroy]
-  resources :categories
-  resources :posts
+  resources :messages, only: [:new, :create]
 
   authenticate :user, -> (u) { u.admin == true } do
     match 'admin' => 'home#admin', via: :get
     resources :messages, only: [:show, :index, :destroy]
+    resources :categories
+    resources :posts, except: :show
   end
+
+  resources :posts, only: :show
 
   %w( 404 422 500 ).each do |code|
     get code, to: 'errors#show', code: code
